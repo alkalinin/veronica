a1a7dbbccd0a21b64f0bf38ab001b4a1acaf3c87
Alexander Kalinin <alec.kalinin@gmail.com>
2019-02-04 10:51:35 +0100

    Update look & feel, draft of the login component

--- lib/src/nav_component.dart
+++ lib/src/nav_component.dart
@@ -1,10 +1,14 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+
+import 'route_paths.dart';
 
 @Component(
   selector: 'roma-nav',
   templateUrl: 'nav_component.html',
   styleUrls: ['nav_component.css'],
-  directives: [coreDirectives],
+  directives: [coreDirectives,  routerDirectives],
+  exports: [RoutePaths]
 )
 class NavComponent {
 }
