cdb456129a77e24e10eca3cc7a2a0f46ac24374d
Alexander Kalinin <alec.kalinin@gmail.com>
2019-02-05 16:17:44 +0100

    Login to google firebase

--- lib/src/login_component.dart
+++ lib/src/login_component.dart
@@ -1,14 +1,38 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:firebase/firebase.dart' as fb;
 
 import "nav_info.dart";
+import "user_service.dart";
 
 @Component(
   selector: 'roma-login',
   templateUrl: 'login_component.html',
   styleUrls: ['login_component.css'],
-  directives: [coreDirectives],
+  directives: [coreDirectives, formDirectives],
 )
 class LoginComponent {
   @Input()
   NavInfo navInfo;
+  UserService _userService;
+
+  String email;
+  String password;
+
+  LoginComponent(this._userService);
+
+  void login() async {
+    print(email);
+    print(password);
+
+    final fb.Auth auth = fb.auth();
+
+    if (email.isNotEmpty && password.isNotEmpty) {
+      try {
+        await auth.signInWithEmailAndPassword(email, password);
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+  }
 }
--- lib/src/user_service.dart
+++ lib/src/user_service.dart
@@ -1,3 +1,23 @@
+import 'package:firebase/firebase.dart' as fb;
+
 class UserService {
   bool isAuthorized = false;
+
+  UserService() {
+    initialize();
+  }
+
+  void initialize() {
+    try {
+      fb.initializeApp(
+        apiKey: 'AIzaSyCOjTrMcjI_0PVGlUtoFGOJ9qnDLgJGbDw',
+        authDomain: 'veronica-roma.firebaseapp.com',
+        databaseURL: 'https://veronica-roma.firebaseio.com',
+        storageBucket: 'veronica-roma.appspot.com');
+
+        print('Initialization OK');    
+    } on fb.FirebaseJsNotLoadedException catch (e) {
+      print(e);
+    }
+  }
 }
